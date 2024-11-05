184d9d17893f39fa5374e96753077e2bba893cb1
Sanish <bsanish86537@gmail.com>
2024-11-05 19:38:33 +0545

    Change text

--- src/components/navbarV2/NavbarV2.js
+++ src/components/navbarV2/NavbarV2.js
@@ -25,7 +25,7 @@ function NavbarV2() {
     <div className="n2-navbar flex items-center justify-between px-20 py-4">
       <div className="font-bold text-lg w-2">Developer.</div>
       {isMediumScreen || isSmallScreen? 
-            <div>loll</div>
+            <div></div>
       :
       <ul className="n2-nav-menu flex justify-center gap-10 pt-2 pb-2 rounded-3xl">
         {navItems.map((item, index) => (
